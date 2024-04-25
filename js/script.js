14ec9514a641bd234480cdacb4394536ca98bec4
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-04-25 07:46:12 +0200

    Imported project data

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const newText = $("<p>new text</p>")
-$("body").append(newText)
-
-const newElement = document.createElement("p")
-newElement.innerText = "new p line"
-document.querySelector("main").appendChild(newElement)
-
-console.log("hello")
